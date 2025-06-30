adf1737abcc39af6911a45dff8aa3654b555a01a
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-06-30 12:06:29 +0530

    2634. Filter Elements from Array

---
+++ JavaScript/2634FilterElementsfromArray.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+// 2634. Filter Elements from Array
+
+var filter = function (arr, fn) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+
+console.log(filter([0, 10, 20, 30], (n) => n > 10));   // [20, 30]
+console.log(filter([1, 2, 3], (n, i) => i === 0));     // [1]
+console.log(filter([-2, -1, 0, 1, 2], (n) => n + 1));  // [-2, 0, 1, 2]
