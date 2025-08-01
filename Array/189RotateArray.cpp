e9bc44827161d92aa54342022a3e2fa4b702a3fe
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-08-01 21:59:24 +0530

    118. Pascal's Triangle

---
+++ Array/189RotateArray.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void rotate(vector<int> &nums, int k)
+    {
+        k = k % nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector nums = {1, 1, 2, 2, 2, 3, 3, 4, 5, 5};
+    int k = 3;
+    sol.rotate(nums, k);
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << " ";
+    }
+    return 0;
+}
