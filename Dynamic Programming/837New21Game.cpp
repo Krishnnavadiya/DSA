732b2e13ae2898ca619f98c8157bb9c17cadc8a7
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-08-18 14:33:46 +0530

    837. New 21 Game

---
+++ Dynamic Programming/837New21Game.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 837. New 21 Game
+
+class Solution
+{
+public:
+    double new21Game(int n, int k, int maxPts)
+    {
+        if (k == 0 || n >= k - 1 + maxPts)
+            return 1.0;
+
+        vector<double> dp(maxPts, 0.0);
+        dp[0] = 1.0;
+
+        double windowSum = 1.0, result = 0.0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            double prob = windowSum / maxPts;
+
+            if (i < k)
+            {
+                windowSum += prob;
+            }
+            else
+            {
+                result += prob;
+            }
+
+            if (i >= maxPts)
+            {
+                windowSum -= dp[i % maxPts];
+            }
+
+            dp[i % maxPts] = prob;
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    cout << sol.new21Game(10, 1, 10);
+    return 0;
+}
