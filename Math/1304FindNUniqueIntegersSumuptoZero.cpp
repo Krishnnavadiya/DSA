485b7cdd41b2594568e29ea9b5f5c979756fec1f
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-09-07 14:23:17 +0530

    1304. Find N Unique Integers Sum up to Zero

---
+++ Math/1304FindNUniqueIntegersSumuptoZero.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1304. Find N Unique Integers Sum up to Zero
+
+class Solution
+{
+public:
+    vector<int> sumZero(int n)
+    {
+        vector<int> result;
+        for (int i = 1; i <= n / 2; i++)
+        {
+            result.push_back(i);
+            result.push_back(-i);
+        }
+        if (n % 2 == 1)
+        {
+            result.push_back(0);
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> arr = sol.sumZero(5);
+    for (int i : arr)
+    {
+        cout << i;
+    }
+    return 0;
+}
