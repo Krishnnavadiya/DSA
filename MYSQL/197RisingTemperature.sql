44ef2397448df6a4acd2e7ba068833c3c5c861b1
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-09-19 22:51:51 +0530

    197. Rising Temperature

---
+++ MYSQL/197RisingTemperature.sql
@@ -0,0 +1,19 @@
+Create table If Not Exists Weather (id int, recordDate date, temperature int) Truncate table Weather
+insert into Weather (id, recordDate, temperature)
+values ('1', '2015-01-01', '10')
+insert into Weather (id, recordDate, temperature)
+values ('2', '2015-01-02', '25')
+insert into Weather (id, recordDate, temperature)
+values ('3', '2015-01-03', '20')
+insert into Weather (id, recordDate, temperature)
+values ('4', '2015-01-04', '30') 
+
+
+-- 197.Rising Temperature # Write your MySQL query statement below
+
+
+SELECT today.id
+FROM Weather yesterday
+    CROSS JOIN Weather today
+WHERE DATEDIFF(today.recordDate, yesterday.recordDate) = 1
+    AND today.temperature > yesterday.temperature;
